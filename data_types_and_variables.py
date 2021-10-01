acb60f8f9716ec0400b7beb33d8e33ab209513b2
masonsherbondy <sherbondy.mason@gmail.com>
2021-10-01 17:17:42 -0500

    whoops forgot this one

---
+++ data_types_and_variables.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+days_with_movies = {
+    "The Little Mermaid": 3,
+    "Brother Bear": 5,
+    "Hercules": 1
+}
+
+price_per_day = 3
+Total_price = (days_with_movies["The Little Mermaid"] + days_with_movies["Brother Bear"] + days_with_movies["Hercules"]) * price_per_day
+
+
+# In[2]:
+
+
+Total_price
+
+
+# In[5]:
+
+
+google = 400
+amazon = 380
+facebook = 350
+payment = 10 * facebook + 6 * google + 4 * amazon
+print(payment)
+
+
+# In[7]:
+
+
+does_class_have_room = True
+does_class_fit_in_schedule = True
+can_be_enrolled = does_class_have_room and does_class_fit_in_schedule
+print(can_be_enrolled)
+
+
+# In[11]:
+
+
+bought_more_than_2 = True or False
+is_offer_to_date = True or False
+is_customer_member = True or False
+can_apply_offer = bought_more_than_2 or is_customer_member and is_offer_to_date
+
+
+# In[12]:
+
+
+can_apply_offer
+
+
+# In[28]:
+
+
+username = 'codeup'
+password = 'notastrongpassword'
+print(len(password) > 5)
+print(len(username) < 20)
+print(username != password)
+print(username[0] and username[-1] and password[0] and password[-1] not in ' ')
+
+
+# In[27]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

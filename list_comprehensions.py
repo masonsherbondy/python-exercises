afe8798b3ae55a1dd1b98c6438fb87bd3620f828
masonsherbondy <sherbondy.mason@gmail.com>
2021-10-01 17:43:56 -0500

    minus bonus

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -33,7 +33,7 @@ fruits_with_more_than_two_vowels = [fruit for fruit in fruits if sum(1 for x in 
 
 
 #Exercise 4
-fruits_with_only_two_vowels = [fruit for fruit in fruits if sum(1 for x in fruit if x in 'aieou') ==2]
+fruits_with_only_two_vowels = [fruit for fruit in fruits if sum(1 for x in fruit if x in 'aieou') == 2]
 
 
 # In[6]:
@@ -64,8 +64,71 @@ fruits_with_less_than_five_characters = [fruit for fruit in fruits if sum(1 for 
 number_of_characters_in_fruits = [len(fruit) for fruit in fruits]
 
 
-# In[ ]:
+# In[10]:
 
 
 #Exercise 9
+fruits_with_letter_a = [fruit for fruit in fruits if any(x == 'a' for x in fruit)]
+
+
+# In[11]:
+
+
+#Exercise 10
+even_numbers = [n for n in numbers if n % 2 == 0]
+
+
+# In[13]:
+
+
+#Exercise 11
+odd_numbers = [n for n in numbers if n % 2 != 0]
+
+
+# In[14]:
+
+
+#Exercise 12
+positive_numbers = [n for n in numbers if n > 0]
+
+
+# In[15]:
+
+
+#Exercise 13
+negative_numbers = [n for n in numbers if n < 0]
+
+
+# In[18]:
+
+
+#Exercise 14
+multiple_digits = [n for n in numbers if abs(n) > 9]
+
+
+# In[19]:
+
+
+#Exercise 15
+numbers_squared = [n ** 2 for n in numbers]
+
+
+# In[20]:
+
+
+#Exercise 16
+odd_negative_numbers = [n for n in numbers if n < 0 and n % 2 != 0]
+
+
+# In[21]:
+
+
+#Exercise 17
+numbers_plus_5 = [n + 5 for n in numbers]
+
+
+# In[ ]:
+
+
+
 

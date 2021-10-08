e716ceef34c854b7dee09bbbe14d65a48eaa80f0
masonsherbondy <sherbondy.mason@gmail.com>
2021-10-08 00:54:24 -0500

    omg imports

---
+++ import_exercises.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Exercise 1b
+from function_exercises import calculate_tip
+print(calculate_tip(.35, 20))
+
+
+# In[ ]:
+
+
+#Exercise 1c
+from function_exercises import get_letter_grade as glg
+glg(82)
+
+
+# In[ ]:
+
+
+#Exercise 2
+import itertools as it
+
+
+# In[ ]:
+
+
+#Exercise 2a
+z = it.permutations('abc123')
+count = 0
+for g in z:
+    count +=1
+count
+
+
+# In[ ]:
+
+
+#Exercise 2b
+x = it.combinations('abcd', 2)
+count = 0
+for c in x:
+    count +=1
+count
+
+
+# In[ ]:
+
+
+#Exercise 2c
+y = it.permutations('abcd', 2)
+count = 0
+for p in y:
+    count += 1
+count
+
+
+# In[ ]:
+
+
+#Exercise 3
+import json
+json.load(open('profiles.json'))
+
+
+# In[ ]:
+
+
+#Exercise 3i
+profiles = json.load(open('profiles.json'))
+profiles[0]
+count = 0
+for user in range(len(profiles)):
+    count += 1
+count
+
+
+# In[ ]:
+
+
+#Exercise 3ii
+count = 0
+for user in profiles:
+    if user['isActive']:
+        count += 1
+count
+
+
+# In[ ]:
+
+
+#Exercise 3iii
+count = 0
+for users in profiles:
+    if not users['isActive']:
+        count += 1
+count
+
+
+# In[ ]:
+
+
+#Exercise 3iv
+from function_exercises import handle_commas as hc
+grand_total = 0
+for user in profiles:
+    grand_total += hc(user['balance'])
+grand_total
+
+
+# In[ ]:
+
+
+#Exercise 3v
+grand_total = 0
+for user in profiles:
+    grand_total += hc(user['balance'])
+avg = grand_total / len(profiles)
+avg
+
+
+# In[ ]:
+
+
+#Exercise 3vi
+lowest_balance = min(profiles, key = lambda user: user['balance'])
+lowest_balance
+lowest_balance['name']
+
+
+# In[ ]:
+
+
+#Exercise 3vii
+highest_balance = max(profiles, key = lambda user: user['balance'])
+highest_balance['name']
+
+
+# In[ ]:
+
+
+#Exercise 3viii & 3ix
+fruits = []
+for user in profiles:
+    fruits += user['favoriteFruit']
+print(fruits)
+strawberries = 0
+apples = 0
+bananas = 0
+for user in profiles:
+    if user['favoriteFruit'] == 'strawberry':
+        strawberries += 1
+    elif user['favoriteFruit'] == 'apple':
+        apples += 1
+    else:
+        bananas += 1
+
+fruit_comparison = {
+    'strawberries': strawberries,
+    'apples': apples,
+    'bananas': bananas
+}
+
+max(fruit_comparison)
+min(fruit_comparison)
+
+
+# In[ ]:
+
+
+#Exercise 3x
+import re
+from itertools import chain
+def unread_messages(x):
+    unread = []
+    for user in profiles:
+        unread.append(re.findall(r'\d+', user['greeting']))
+    unread_int = list(chain(*unread))
+    for n in range(len(unread_int)):
+        unread_int[n] = int(unread_int[n])
+    return sum(unread_int)
+
+unread_messages(profiles)
+
